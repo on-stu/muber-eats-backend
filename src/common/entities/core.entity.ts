3a8b49119fa4cedbbf46598fa619f3556dda1dbc
김민수 <kimminsu@gimminsuui-MacBookAir.local>
2022-08-10 16:23:18 +0900

    error management

--- src/common/entities/core.entity.ts
+++ src/common/entities/core.entity.ts
@@ -1,12 +1,16 @@
+import { Field } from '@nestjs/graphql';
 import { CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 export class CoreEntity {
   @PrimaryGeneratedColumn()
+  @Field((type) => Number)
   id: number;
 
   @CreateDateColumn()
+  @Field((type) => Date)
   createdAt: Date;
 
   @CreateDateColumn()
+  @Field((type) => Date)
   updatedAt: Date;
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,8 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 import { User } from './entities/user.entitiy';
 import { UsersService } from './users.service';
 
@@ -9,5 +13,26 @@ export class UsersResolver {
   @Query((returns) => Boolean)
   hi() {
     return true;
+  }
+
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const { ok, error } = await this.usersService.createAccount(
+        createAccountInput,
+      );
+
+      return {
+        ok,
+        error,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error: error,
+      };
+    }
   }
 }
